43705d17c957441e7ed5f77e0a979343d477f569
JAlejandroQuevedo <alejandro_ya23@hotmail.com>
2024-03-13 06:30:36 -0700

    main routes diferentes

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,22 +7,15 @@ import { Layout } from '../components/layout/Layout';
 import { CheckoutContainer } from '../components/pages/chekout/CheckoutContainer';
 import CartContextProvider from '../context/CartContext';
 import { CartContainer } from '../components/common/Cart/CartContainer';
+import { routes } from '../router/routes';
+import { AppRouter } from '../router/AppRouter';
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <CartContextProvider>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<ItemListContainer />} />
-            <Route path='/category/:category' element={<ItemListContainer />} />
-            <Route path='/cart' element={<CartContainer />} />
-            <Route path='/item/:id' element={<ItemDetailContainer />} />
-            <Route path='/checkout' element={<CheckoutContainer />} />
-          </Route>
-          <Route path='*' element={<h1>404 not found</h1>} />
-        </Routes>
+        <AppRouter />
       </CartContextProvider>
     </BrowserRouter>
 
